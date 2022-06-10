3540e53538dd1734ad4113d386eb722ba7721d75
unknown <leonardellilucas@gmail.com>
2022-06-10 12:51:58 -0300

    finish commit

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -5,7 +5,8 @@ Definir las funciones recursivas nFactorial y nFibonacci.
 
 nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma,
+ y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
@@ -14,10 +15,37 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
+
+
+
+
 function nFactorial(n) {
+
+  if ( n > -1 && n < 2){
+      return 1;
+
+  }else if ( n === 0){
+
+    return 'Error, no se puede sacar el factorial de un numero 0'
+  }
+
+  return n * nFactorial (n -1);
+
 }
 
-function nFibonacci(n) {
+// nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma,
+//  y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+// Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
+
+// Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+
+function nFibonacci(n) {   //
+  if ( n<2){
+    return n;
+  }else{
+    return (nFibonacci(n-1) + nFibonacci(n-2))
+  }
+  
 }
 
 /*
@@ -29,9 +57,47 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {
+class  Queue {
+   
+    constructor( ) {
+      
+      this.arr = [];
 
-}
+    }
+    enqueue ( elemento ) {
+      this.arr.push(elemento);
+    }
+
+    dequeue (){
+      if(this.arr.length === 0 ){
+        return undefined;
+      }
+       return this.arr.shift();
+    }
+
+    size ( ){
+      return  this.arr.length;
+    }
+
+  }
+ 
+
+
+
+
+
+
+  
+
+
+   
+  
+
+
+
+    
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
